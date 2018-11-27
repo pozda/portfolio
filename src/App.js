a6e942b85b0b2c873cb3278bfcf9ac2a1e17a6a7
Ivan Pozderac <ivan.pozderac@gmail.com>
2018-11-27 06:59:28 +0100

    add google analytics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 // @flow
 import * as React from 'react'
 import 'UI/Styles/Main.scss'
+import * as ReactGA from 'react-ga'
 
 type Props = {
     children: ?React.Node
 }
 
 class App extends React.Component<Props> {
+    componentDidMount(): void {
+        this.initializeReactGA()
+    }
+
+    initializeReactGA() {
+        ReactGA.initialize('UA-129899447-1')
+        ReactGA.pageview('index')
+    }
+
     render() {
         return (
             <React.Fragment>
